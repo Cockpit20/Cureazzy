9e7c22531fbb840d42a0ea29cbead80414486b91
Soumyadeep Chandra <soumyadeepchandra2002@gmail.com>
2023-08-01 20:29:01 +0530

    Updated server, env and login files

--- Cureazy-Frontend-master/src/Pages/Login.js
+++ Cureazy-Frontend-master/src/Pages/Login.js
@@ -1,71 +1,115 @@
 import React from 'react';
-import { FcGoogle } from 'react-icons/fc';
+// import { FcGoogle } from 'react-icons/fc';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import jwt_decode from 'jwt-decode';
+import { useState, useEffect } from 'react';
 import axios from 'axios'
 const Login = () => {
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleLogin = ()=>{
+    useEffect(() => {
+
+        window.google.accounts.id.initialize({
+            client_id: "957413395116-9415jluo91928e8kfvrjthvsnvhp6ib7.apps.googleusercontent.com",
+            callback: handleGoogleSignUp
+        })
+
+        window.google.accounts.id.renderButton(
+            document.getElementById('signup'),
+            {
+                theme: "outline",
+                width: "344px"
+            }
+        )
+
+        window.google.accounts.id.prompt()
+
+    }, [])
+
+    const handleLogin = () => {
         axios({
-        method: 'post',
-        url: "http://localhost:5000/login",
-        headers: {}, 
-        data: {
-            "email": email,
-            "password": password
-        }
-        }).then((res)=>{
+            method: 'post',
+            url: "http://localhost:5000/login",
+            headers: {},
+            data: {
+                "email": email,
+                "password": password
+            }
+        }).then((res) => {
             console.log(res)
-            alert("login successful!")
-        }).catch((err)=>{
+            alert("Login successful!")
+        }).catch((err) => {
             console.error(err)
             alert("Some Error Occured!")
         })
-  }
+    }
 
-  const loginImg = 'https://images.unsplash.com/photo-1550831106-0994fe8abcfe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'
+    const handleGoogleSignUp = async (res) => {
+        console.log(res.credential)
+        var user = jwt_decode(res.credential)
+        console.log(user.email)
 
-  return (    
-    <div className="register-container">
-        <div className="register-bg">
-            <div className="register-left">
-                <img src={loginImg} alt="" />
-            </div>
-            <div className="register-right">
-                <div className='section-container register-form'>
-                    <h2>Welcome Back</h2>
-                    <button className='register-form-btn'><FcGoogle/> Log In with Google</button>
-                    <div className="divider">
-                        <hr/>
+        axios({
+            method: 'post',
+            url: "http://localhost:5000/login/google",
+            headers: {},
+            data: {
+                "email": user.email
+            }
+        }).then((res) => {
+            console.log(res)
+            alert("Login Successful!")
+        }).catch((err) => {
+            console.log(err)
+            alert(err.response.data.error)
+        })
+
+        alert(`Welcome ${user.name}!`)
+    }
+
+    const loginImg = 'https://images.unsplash.com/photo-1550831106-0994fe8abcfe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'
+
+    return (
+        <div className="register-container">
+            <div className="register-bg">
+                <div className="register-left">
+                    <img src={loginImg} alt="" />
+                </div>
+                <div className="register-right">
+                    <div className='section-container register-form'>
+                        <h2>Welcome Back</h2>
+                        <div id="signup"></div>
+                        {/* <button className='register-form-btn'><FcGoogle/> Log In with Google</button> */}
+                        <div className="divider">
+                            <hr />
                             <span>or</span>
-                        <hr/>
+                            <hr />
+                        </div>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button className='register-form-btn register-submit' onClick={handleLogin}>
+                            Log In
+                        </button>
+                        <p>
+                            Don't Have an Account? <Link to="/signup">Sign Up</Link>
+                        </p>
                     </div>
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button className='register-form-btn register-submit' onClick={handleLogin}>
-                        Log In
-                    </button>
-                    <p>
-                        Don't Have an Account? <Link to="/signup">Sign Up</Link>
-                    </p>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Login;
